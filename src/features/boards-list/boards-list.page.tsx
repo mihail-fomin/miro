61c7f1615566e81ced106185fe11f559d3db3aaf
Mihail Fomin <m.fomin@quickresto.ru>
2025-05-19 15:38:29 +0500

    Added openapi, msw

--- src/features/boards-list/boards-list.page.tsx
+++ src/features/boards-list/boards-list.page.tsx
@@ -1,12 +1,63 @@
 import { ROUTES } from "@/shared/model/routes";
 import { href, Link } from "react-router-dom";
+import { rqClient } from "@/shared/api/instance";
+import { queryClient } from "@/shared/api/query-client";
 
 function BoardsListPage() {
+  const boardsQuery = rqClient.useQuery("get", "/boards");
+
+  const createBoardMutation = rqClient.useMutation("post", "/boards", {
+    onSettled: async () => {
+      await queryClient.invalidateQueries({ queryKey: ["get", "/boards"] });
+    },
+  });
+  const deleteBoardMutation = rqClient.useMutation(
+    "delete",
+    "/boards/{boardId}",
+    {
+      onSettled: async () => {
+        await queryClient.invalidateQueries({ queryKey: ["get", "/boards"] });
+      },
+    },
+  );
+
   return (
     <div>
       <h1>Boards list</h1>
 
-      <Link to={href(ROUTES.BOARD, { boardId: "1" })}>Board 1</Link>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const formData = new FormData(e.target as HTMLFormElement);
+
+          createBoardMutation.mutate({
+            body: { name: formData.get("name") as string },
+          });
+        }}
+      >
+        <input type="text" name="name" />
+        <button type="submit" disabled={createBoardMutation.isPending}>
+          {createBoardMutation.isPending ? "Creating..." : "Create"}
+        </button>
+      </form>
+
+      {boardsQuery.data?.map((board) => (
+        <div key={board.id}>
+          <Link to={href(ROUTES.BOARD, { boardId: board.id })}>
+            {board.name}
+          </Link>
+          <button
+            disabled={deleteBoardMutation.isPending}
+            onClick={() =>
+              deleteBoardMutation.mutate({
+                params: { path: { boardId: board.id } },
+              })
+            }
+          >
+            Delete
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
