f9ef1871e69b1264adfd7fb166c86ea7807d518e
Mihail Fomin <m.fomin@quickresto.ru>
2025-05-21 18:34:17 +0500

    Added shadcn

--- src/features/boards-list/boards-list.page.tsx
+++ src/features/boards-list/boards-list.page.tsx
@@ -2,6 +2,8 @@ import { ROUTES } from "@/shared/model/routes";
 import { href, Link } from "react-router-dom";
 import { rqClient } from "@/shared/api/instance";
 import { queryClient } from "@/shared/api/query-client";
+import { Card, CardFooter, CardHeader } from "@/shared/ui/kit/card";
+import { Button } from "@/shared/ui/kit/button";
 
 function BoardsListPage() {
   const boardsQuery = rqClient.useQuery("get", "/boards");
@@ -22,7 +24,7 @@ function BoardsListPage() {
   );
 
   return (
-    <div>
+    <div className="container mx-auto p-4">
       <h1>Boards list</h1>
 
       <form
@@ -41,23 +43,32 @@ function BoardsListPage() {
         </button>
       </form>
 
-      {boardsQuery.data?.map((board) => (
-        <div key={board.id}>
-          <Link to={href(ROUTES.BOARD, { boardId: board.id })}>
-            {board.name}
-          </Link>
-          <button
-            disabled={deleteBoardMutation.isPending}
-            onClick={() =>
-              deleteBoardMutation.mutate({
-                params: { path: { boardId: board.id } },
-              })
-            }
-          >
-            Delete
-          </button>
-        </div>
-      ))}
+      <div className="grid grid-cols-3 gap-4">
+        {boardsQuery.data?.map((board) => (
+          <Card key={board.id}>
+            <CardHeader>
+              <Button variant="link" asChild>
+                <Link to={href(ROUTES.BOARD, { boardId: board.id })}>
+                  {board.name}
+                </Link>
+              </Button>
+            </CardHeader>
+            <CardFooter>
+              <Button
+                variant="destructive"
+                disabled={deleteBoardMutation.isPending}
+                onClick={() =>
+                  deleteBoardMutation.mutate({
+                    params: { path: { boardId: board.id } },
+                  })
+                }
+              >
+                Delete
+              </Button>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
