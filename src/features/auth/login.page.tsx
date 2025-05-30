d71e2c3575afb4c85b30c5cd25637b61fb5b0b07
Misha <mihail_fomin_95@mail.ru>
2025-05-30 17:20:28 +0500

    Added auth mocks

--- src/features/auth/login.page.tsx
+++ src/features/auth/login.page.tsx
@@ -1,4 +1,12 @@
+import { rqClient } from "@/shared/api/instance";
+
 function LoginPage() {
+    // const loginMutation = rqClient.useMutation('post', '/auth/login')
+
+    // loginMutation.mutate({
+    //     email: 'test@test.com',
+    //     password: 'test',
+    // })
   return <div>Login page</div>;
 }
 
