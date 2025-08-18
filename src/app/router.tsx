1ff9b1a3e0fba996c31f2cefce119f1298f9b013
Misha <mihail_fomin_95@mail.ru>
2025-08-18 22:01:24 +0500

    Moved header to router

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter, redirect } from "react-router-dom";
 import { App } from "./app";
 import { Providers } from "./providers";
 import { ProtectedRoute } from "./protected-route";
+import { AppHeader } from "@/features/header";
 
 export const router = createBrowserRouter([
   {
@@ -13,7 +14,12 @@ export const router = createBrowserRouter([
     ),
     children: [
       {
-        Component: ProtectedRoute,
+        element: (
+          <>
+            <AppHeader />
+            <ProtectedRoute />
+          </>
+        ),
         children: [
           {
             path: ROUTES.BOARDS,
