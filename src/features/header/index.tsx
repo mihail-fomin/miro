7aff74f5efceccdfaa5505741d5d944cb581e964
Misha <mihail_fomin_95@mail.ru>
2025-08-19 21:53:09 +0500

    Removed header when no session

--- src/features/header/index.tsx
+++ src/features/header/index.tsx
@@ -1,16 +1,16 @@
-import { ROUTES } from "@/shared/model/routes";
 import { useSession } from "@/shared/model/session";
-import { Link } from "react-router-dom";
 import { Button } from "@/shared/ui/kit/button";
 
 export function AppHeader() {
   const { session, logout } = useSession();
+
+  if (!session) return null
+
   return (
     <header className="bg-background border-b border-border/40 shadow-sm py-3 px-4 mb-6">
       <div className="max-w-7xl mx-auto flex items-center justify-between">
         <div className="text-xl font-semibold">Miro Copy</div>
 
-        {session ? (
           <div className="flex items-center gap-4">
             <span className="text-sm text-muted-foreground">
               {session.email}
@@ -24,11 +24,6 @@ export function AppHeader() {
               Выйти
             </Button>
           </div>
-        ) : (
-          <Button asChild variant="default" size="sm">
-            <Link to={ROUTES.LOGIN}>Войти</Link>
-          </Button>
-        )}
       </div>
     </header>
   );
