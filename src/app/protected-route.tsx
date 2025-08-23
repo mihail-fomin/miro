d1240dc714627b72a9961981d23e552fcd14e639
Misha <mihail_fomin_95@mail.ru>
2025-08-23 13:23:19 +0500

    Added protected loader

--- src/app/protected-route.tsx
+++ src/app/protected-route.tsx
@@ -1,6 +1,7 @@
 import { ROUTES } from "@/shared/model/routes";
 import { useSession } from "@/shared/model/session";
 import { Navigate, Outlet } from "react-router-dom";
+import { enableMocking } from "@/shared/api/mocks";
 
 export function ProtectedRoute() {
   const { session } = useSession();
@@ -11,4 +12,16 @@ export function ProtectedRoute() {
   }
 
   return <Outlet />;
+}
+
+export async function protectedLoader() {
+  await enableMocking();
+
+  const token = await useSession.getState().refreshToken();
+
+  if (!token) {
+    return <Navigate to={ROUTES.LOGIN} />;
+  }
+
+  return null;
 }
---
+++ src/shared/api/mocks/index.ts
@@ -0,0 +1,9 @@
+export async function enableMocking() {
+    if (import.meta.env.PROD) {
+      return;
+    }
+  
+    const { worker } = await import("@/shared/api/mocks/browser");
+    return worker.start();
+  }
+  
